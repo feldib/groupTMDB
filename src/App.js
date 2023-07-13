64529399cafccddb22a8614c70ce47f51af716e3
feldib <beni.feldmayer@gmail.com>
2023-07-13 18:08:31 +0200

    adding routing structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
-import Homepage from './Homepage';
-import Header from "./Header";
-import Footer from "./Footer";
-import MoviePage from './MoviePage';
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
+import Header from './Header'
+import Footer from './Footer'
+import Homepage from './Homepage';
+import Search from './Search'
+import ProfilePage from './ProfilePage'
+import Details from './Details';
+import ViewHistory from './ViewHistory';
+import LoginPage from './LoginPage'
+import RegisterPage1 from './RegisterPage1';
+import RegisterPage2 from './RegisterPage3';
+import RegisterPage3 from './RegisterPage3';
 
 function App() {
   return (
-    <div class="main">
+    <div>
       <Header />
-      <MoviePage />
+        <Routes>
+          <Route path="/" element={
+              <Homepage />
+            } 
+          />
+          <Route path="Search" element={
+            <Search />
+          } />
+
+          <Route path="ProfilePage" element={
+            <ProfilePage />
+          }>
+            <Route path="Details" element={
+              <Details />
+            }
+            />
+            <Route path="ViewHistory" element={
+              <ViewHistory />
+            }
+            />
+          </Route>
+
+          <Route path="LoginPage" element={
+            <LoginPage />
+          } />
+
+          <Route path="RegisterPage1" element={
+            <RegisterPage1 />
+          } />
+
+          <Route path="RegisterPage2" element={
+            <RegisterPage2 />
+          } />
+
+          <Route path="RegisterPage3" element={
+            <RegisterPage3 />
+          } />
+        </Routes>
       <Footer />
     </div>
+              
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightToBracket, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 import logo from "./logo.svg"
 
 function Header() {
@@ -17,17 +18,19 @@ function Header() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home Page <span className="sr-only">(current)</span></a>
+                            <Link className="nav-link" to="/">Home Page</Link>
                         </li>
-
                         <li classname="nav-item">
-                            <a className="nav-link" href="#">Search a movie</a>
+                            <Link className="nav-link" to="Search">Search a movie</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Register/Login</a>
+                            <Link className="nav-link" to="Register1">Register</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Profile</a>
+                            <Link className="nav-link" to="LoginPage">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="ProfilePage">Profile</Link>
                         </li>
                     </ul>
                     <div className="row justify-content-md-center justify-content-start mt-3 mb-2 ml-1 mr-2">
