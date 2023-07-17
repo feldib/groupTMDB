658fb71251ac360c3de5cf6e2d0994e8f8329f4d
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-07-17 17:17:37 +0100

    Adding local storage to registration page

--- src/RegisterPage2.js
+++ src/RegisterPage2.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Form, InputGroup, Row, Col } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLanguage, faFilm } from "@fortawesome/free-solid-svg-icons"
 import { Link } from 'react-router-dom'
 
 const RegisterPage2 = () => {
+    const [languageForMovie, setLanguageForMovie] = useState("");
+    const [favouriteGenre, setFavouriteGenre] = useState("");
+
+    useEffect(() => {
+        localStorage.setItem('languageForMovie', languageForMovie);
+        localStorage.setItem('favouriteGenre', favouriteGenre);
+    }, [languageForMovie, favouriteGenre]);
+
     return (
         <Row className="register-page-2">
             <h1 className='text-center'>Come Register!</h1>
             <Col className='mx-5'>
                 <Form>
-
                     <Form.Group className="mb-3" controlId="formLanguage">
                         <Form.Label>Language:</Form.Label>
                         <InputGroup>
                             <InputGroup.Text><FontAwesomeIcon icon={faLanguage} /></InputGroup.Text>
-                            <Form.Control type="text" placeholder="Enter Language" />
+                            <Form.Control type="text" placeholder="Enter Language" value={languageForMovie} onChange={(e) => setLanguageForMovie(e.target.value)} />
                         </InputGroup>
                     </Form.Group>
     
@@ -23,27 +30,27 @@ const RegisterPage2 = () => {
                         <Form.Label>Favourite Genre:</Form.Label>
                         <InputGroup>
                             <InputGroup.Text><FontAwesomeIcon icon={faFilm} /></InputGroup.Text>
-                            <Form.Control as="select" defaultValue="">
-                                <option value="" disabled>Select Genre...</option>
-                                <option value="genre1">Action</option>
-                                <option value="genre2">Adventure</option>
-                                <option value="genre3">Animation</option>
-                                <option value="genre4">Comedy</option>
-                                <option value="genre5">Crime</option>
-                                <option value="genre6">Documentary</option>
-                                <option value="genre7">Drama</option>
-                                <option value="genre8">Family</option>
-                                <option value="genre9">Fantasy</option>
-                                <option value="genre10">History</option>
-                                <option value="genre11">Horror</option>
-                                <option value="genre12">Music</option>
-                                <option value="genre13">Mystery</option>
-                                <option value="genre14">Romance</option>
-                                <option value="genre15">Science Fiction</option>
-                                <option value="genre16">Thriller</option>
-                                <option value="genre17">TV Movie</option>
-                                <option value="genre18">War</option>
-                                <option value="genre19">Western</option>
+                            <Form.Control as="select" value={favouriteGenre} onChange={(e) => setFavouriteGenre(e.target.value)}>
+                            <option value="" disabled>Select Genre...</option>
+                                <option value="Action">Action</option>
+                                <option value="Adventure">Adventure</option>
+                                <option value="Animation">Animation</option>
+                                <option value="Comedy">Comedy</option>
+                                <option value="Crime">Crime</option>
+                                <option value="Documentary">Documentary</option>
+                                <option value="Drama">Drama</option>
+                                <option value="Family">Family</option>
+                                <option value="Fantasy">Fantasy</option>
+                                <option value="History">History</option>
+                                <option value="Horror">Horror</option>
+                                <option value="Music">Music</option>
+                                <option value="Mystery">Mystery</option>
+                                <option value="Romance">Romance</option>
+                                <option value="Science Fiction">Science Fiction</option>
+                                <option value="Thriller">Thriller</option>
+                                <option value="TV Movie">TV Movie</option>
+                                <option value="War">War</option>
+                                <option value="Western">Western</option>
                             </Form.Control>
                         </InputGroup>
                     </Form.Group>
--- src/RegisterPage3.js
+++ src/RegisterPage3.js
@@ -1,64 +1,78 @@
-import React from 'react'
-import { Button, Form, InputGroup, Container, Row, Col } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Button, Form, InputGroup, Row, Col } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFilm, faHeadphonesAlt } from "@fortawesome/free-solid-svg-icons"
+import { faHeadphonesAlt } from "@fortawesome/free-solid-svg-icons"
 import { Link } from 'react-router-dom'
 
 const RegisterPage3 = () => {
+    const [galleryToShow, setGalleryToShow] = useState([]);
+    const [languageForHearing, setLanguageForHearing] = useState("");
+
+    useEffect(() => {
+        localStorage.setItem('galleryToShow', JSON.stringify(galleryToShow));
+        localStorage.setItem('languageForHearing', languageForHearing);
+    }, [galleryToShow, languageForHearing]);
+
+    const handleCheckboxChange = (e) => {
+        if (e.target.checked) {
+            setGalleryToShow([...galleryToShow, e.target.value]);
+        } else {
+            setGalleryToShow(galleryToShow.filter(item => item !== e.target.value));
+        }
+    }
+
     return (
         <Row className="register-page-3">
-          <h1 className='text-center'>Come Register!</h1>
-          <Col className='mx-5'>
-            <Form>
-              <Form.Group className="mb-3" controlId="formGallery">
-                <Form.Label>Show Galleries:</Form.Label>
-                <InputGroup>
-                  <InputGroup.Text>
-                    <FontAwesomeIcon icon={faFilm} />
-                  </InputGroup.Text>
-                  <InputGroup>
-                    <Col>
-                      <Form.Check 
-                          type="checkbox"
-                          id="gallery1"
-                          label="Most Popular"
-                      />
-                      <Form.Check 
-                          type="checkbox"
-                          id="gallery2"
-                          label="Top Rated"
-                      />
-                      <Form.Check 
-                          type="checkbox"
-                          id="gallery3"
-                          label="Upcoming"
-                      />
-                    </Col>
-                  </InputGroup>
-                </InputGroup>
-              </Form.Group>
+            <h1 className='text-center'>Come Register!</h1>
+            <Col className='mx-5'>
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Choose your galleries:</Form.Label>
+                        <Form.Check 
+                            type="checkbox"
+                            id="gallery1"
+                            value="Most Popular"
+                            checked={galleryToShow.includes("Most Popular")}
+                            onChange={handleCheckboxChange}
+                            label="Most Popular"
+                        />
+                        <Form.Check 
+                            type="checkbox"
+                            id="gallery2"
+                            value="Top Rated"
+                            checked={galleryToShow.includes("Top Rated")}
+                            onChange={handleCheckboxChange}
+                            label="Top Rated"
+                        />
+                        <Form.Check 
+                            type="checkbox"
+                            id="gallery3"
+                            value="Upcoming"
+                            checked={galleryToShow.includes("Upcoming")}
+                            onChange={handleCheckboxChange}
+                            label="Upcoming"
+                        />
+                    </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formTTS">
-                <Form.Label>Text-to-Voice Language:</Form.Label>
-                <InputGroup>
-                  <InputGroup.Text>
-                    <FontAwesomeIcon icon={faHeadphonesAlt} />
-                  </InputGroup.Text>
-                  <Form.Control type="text" placeholder="Enter Language" />
-                </InputGroup>
-              </Form.Group>
+                    <Form.Group className="mb-3" controlId="formTTS">
+                        <Form.Label>Text-to-Voice Language:</Form.Label>
+                        <InputGroup>
+                            <InputGroup.Text>
+                                <FontAwesomeIcon icon={faHeadphonesAlt} />
+                            </InputGroup.Text>
+                            <Form.Control type="text" placeholder="Enter Language" value={languageForHearing} onChange={(e) => setLanguageForHearing(e.target.value)} />
+                        </InputGroup>
+                    </Form.Group>
 
-              <Row className='justify-content-center mt-3'>
-                <Col lg="2">
-                    <Link to="/ProfilePage">
-                        <Button className='text-center mx-auto' variant="primary" type="submit">Register!</Button>
-                    </Link>
-                </Col>
-              </Row>
-              
-            </Form>
-          </Col>
-          
+                    <Row className='justify-content-center mt-3'>
+                        <Col lg="2">
+                            <Link to="/ProfilePage">
+                                <Button className='text-center mx-auto' variant="primary" type="submit">Register!</Button>
+                            </Link>
+                        </Col>
+                    </Row>
+                </Form>
+            </Col>
         </Row>
     )
 }
