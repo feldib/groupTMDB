1db1245443e3fea230193167d2bf9632533da0ba
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-07-17 18:34:17 +0100

    Replacing placeholders with localstorage data

--- src/Details.js
+++ src/Details.js
@@ -1,37 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Row, Form, InputGroup} from 'react-bootstrap'
 import { faUser, faKey, faLanguage, faQuestion, faHeadphonesSimple } from "@fortawesome/free-solid-svg-icons"
 import DetailsFormComponent from './DetailsFormComponent'
 
 function Details() {
+    const [username, setUsername] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [languageForMovie, setLanguageForMovie] = useState(null);
+    const [favouriteGenre, setFavouriteGenre] = useState(null);
+    const [galleryToShow, setGalleryToShow] = useState(null);
+    const [languageForHearing, setLanguageForHearing] = useState(null);
+
+    useEffect(() => {
+        setUsername(localStorage.getItem('username'));
+        setPassword(localStorage.getItem('password'));
+        setLanguageForMovie(localStorage.getItem('languageForMovie'));
+        setFavouriteGenre(localStorage.getItem('favouriteGenre'));
+        setGalleryToShow(JSON.parse(localStorage.getItem('galleryToShow')));
+        setLanguageForHearing(localStorage.getItem('languageForHearing'));
+    }, []);
+
     return (
         <Row>
             <Form>
                 <DetailsFormComponent 
                     label = "Username"
                     icon = {faUser}
-                    placeholder = "[Username]"
+                    placeholder = {username || "[Username]"}
                     type = "text"
                 />
                 
                 <DetailsFormComponent 
                     label = "Password"
                     icon = {faKey}
-                    placeholder = "[****]" 
+                    placeholder = {password || "[****]"} 
                     type = "password"
                 />
 
                 <DetailsFormComponent 
                     label = "Favourite Language"
                     icon = {faLanguage}
-                    placeholder = "[Favourite language]"
+                    placeholder = {languageForMovie || "[Favourite language]"}
                     type = "text"
                 />
 
                 <DetailsFormComponent 
                     label = "Favourite Genre"
                     icon = {faQuestion}
-                    placeholder = "[Favourite genre]"
+                    placeholder = {favouriteGenre || "[Favourite genre]"}
                     type = "text"
                 />
 
@@ -43,6 +59,7 @@ function Details() {
                         name="galleries"
                         type="switch"
                         id="mostPopular"
+                        checked={galleryToShow && galleryToShow.includes("Most Popular")}
                     />
                     <Form.Check
                         inline
@@ -50,6 +67,7 @@ function Details() {
                         name="galleries"
                         type="switch"
                         id="topRated"
+                        checked={galleryToShow && galleryToShow.includes("Top Rated")}
                     />
                     <Form.Check
                         inline
@@ -57,13 +75,14 @@ function Details() {
                         name="galleries"
                         type="switch"
                         id="upcoming"
+                        checked={galleryToShow && galleryToShow.includes("Upcoming")}
                     />            
                 </InputGroup>
 
                 <DetailsFormComponent 
                     label = "Voice-To-Text Language"
                     icon = {faHeadphonesSimple}
-                    placeholder = "[Language]" 
+                    placeholder = {languageForHearing || "[Language]"} 
                     type = "text"
                 />
             </Form>
