51cf653896dd2f5dd07b3b711598efc57c0a7957
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-07-17 13:41:18 +0100

    fixing issues associated with RegisterPage2

--- src/RegisterPage2.js
+++ src/RegisterPage2.js
@@ -1,68 +1,60 @@
 import React from 'react'
-import { Button, Form, InputGroup, Container, Row, Col } from 'react-bootstrap'
+import { Button, Form, InputGroup, Row, Col } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLanguage, faQuestion } from "@fortawesome/free-solid-svg-icons"
-import { useHistory } from 'react-router-dom'
+import { faLanguage, faFilm } from "@fortawesome/free-solid-svg-icons"
 
 const RegisterPage2 = () => {
-    const history = useHistory()
-
-    const nextPage = () => {
-        history.push('/register/3')
-    }
-
     return (
-        <Container className="mt-5">
-            <Row className="justify-content-center">
-                <Col xs={12} sm={8} md={6} lg={4}>
-                    <h1 className="mb-4">Come Register!</h1>
-                    <Form>
-                        <Form.Group controlId="formLanguage">
-                            <Form.Label>Favourite Language (Optional):</Form.Label>
-                            <InputGroup>
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text><FontAwesomeIcon icon={faLanguage} /></InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <Form.Control type="text" placeholder="Enter Language" />
-                            </InputGroup>
-                        </Form.Group>
+        <Row className="register-page-2">
+            <h1 className='text-center'>Come Register!</h1>
+            <Col className='mx-5'>
+                <Form>
 
-                        <Form.Group controlId="formGenre">
-                            <Form.Label>Favourite Genre (Optional):</Form.Label>
-                            <InputGroup>
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text><FontAwesomeIcon icon={faQuestion} /></InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <Form.Control as="select">
-                                    <option>Choose Genre...</option>
-                                    <option>Action</option>
-                                    <option>Adventure</option>
-                                    <option>Animation</option>
-                                    <option>Comedy</option>
-                                    <option>Crime</option>
-                                    <option>Documentary</option>
-                                    <option>Drama</option>
-                                    <option>Family</option>
-                                    <option>Fantasy</option>
-                                    <option>History</option>
-                                    <option>Horror</option>
-                                    <option>Music</option>
-                                    <option>Mystery</option>
-                                    <option>Romance</option>
-                                    <option>Science Fiction</option>
-                                    <option>Thriller</option>
-                                    <option>TV Movies</option>
-                                    <option>War</option>
-                                    <option>Western</option>
-                                </Form.Control>
-                            </InputGroup>
-                        </Form.Group>
+                    <Form.Group className="mb-3" controlId="formLanguage">
+                        <Form.Label>Language:</Form.Label>
+                        <InputGroup>
+                            <InputGroup.Text><FontAwesomeIcon icon={faLanguage} /></InputGroup.Text>
+                            <Form.Control type="text" placeholder="Enter Language" />
+                        </InputGroup>
+                    </Form.Group>
+    
+                    <Form.Group className="mb-3" controlId="formGenre">
+                        <Form.Label>Favourite Genre:</Form.Label>
+                        <InputGroup>
+                            <InputGroup.Text><FontAwesomeIcon icon={faFilm} /></InputGroup.Text>
+                            <Form.Control as="select" defaultValue="">
+                                <option value="" disabled>Select Genre...</option>
+                                <option value="genre1">Action</option>
+                                <option value="genre2">Adventure</option>
+                                <option value="genre3">Animation</option>
+                                <option value="genre4">Comedy</option>
+                                <option value="genre5">Crime</option>
+                                <option value="genre6">Documentary</option>
+                                <option value="genre7">Drama</option>
+                                <option value="genre8">Family</option>
+                                <option value="genre9">Fantasy</option>
+                                <option value="genre10">History</option>
+                                <option value="genre11">Horror</option>
+                                <option value="genre12">Music</option>
+                                <option value="genre13">Mystery</option>
+                                <option value="genre14">Romance</option>
+                                <option value="genre15">Science Fiction</option>
+                                <option value="genre16">Thriller</option>
+                                <option value="genre17">TV Movie</option>
+                                <option value="genre18">War</option>
+                                <option value="genre19">Western</option>
+                            </Form.Control>
+                        </InputGroup>
+                    </Form.Group>
 
-                        <Button variant="primary" type="button" block onClick={nextPage}>Next Page</Button>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
+                    <Row className='justify-content-center mt-3'>
+                        <Col lg="2">
+                            <Button className='text-center mx-auto' variant="primary" type="submit">Next Page</Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </Col>
+        </Row>
     )
 }
 
