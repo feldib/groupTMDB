e34b4690ecca9c08b7ce10dae5b8f6dd84d76a57
Katie <e.v.rassolenko@gmail.com>
2023-07-17 17:13:00 +0300

    search bug solved

--- src/MovieGalleryBig.js
+++ src/MovieGalleryBig.js
@@ -4,7 +4,7 @@ import MovieBox from "./MovieBox1"
 function MovieGalleryBig(props) {
     return (
         <div className='d-flex p-3 flex-wrap'>
-            {props.movies.map((movie) => <MovieBox key={movie.id} {...movie} />)}
+            {props.movies.map((movie) => <MovieBox key={movie.id} theMovie={movie} />)}
         </div>
     )
 }
