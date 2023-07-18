5083a407bd9756ff8e9b83a819bfb3d6f4d9dbea
feldib <beni.feldmayer@gmail.com>
2023-07-18 17:43:37 +0200

    sorting, filtering, and searching now all work - however sorting and the other two do not work together yet

--- src/MovieGalleryBig.js
+++ src/MovieGalleryBig.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import MovieBox from "./MovieBox1"
+import {sortingData} from "./sortingOptions"
 
 function MovieGalleryBig(props) {
+    const [orderedMovies, setOrderedMovies] = React.useState(props.movies)
+
+    React.useEffect(()=>{
+        if(props.sorting !== ""){
+            const sortingFunction = sortingData[props.sorting]
+            setOrderedMovies(sortingFunction(props.movies))
+        }else{
+            setOrderedMovies(props.movies)
+        }
+    }, [props.sorting, props.movies])
+
     return (
         <div className='d-flex p-3 flex-wrap'>
-            {props.movies.map((movie) => <MovieBox key={movie.id} theMovie={movie} />)}
+            {orderedMovies.map((movie) => <MovieBox key={movie.id} theMovie={movie} />)}
         </div>
     )
 }
---
+++ src/genres.js
@@ -0,0 +1,22 @@
+const genres = [
+    "Adventure",
+    "Animation",
+    "Comedy",
+    "Crime",
+    "Documentary",
+    "Drama",
+    "Family",
+    "Fantasy",
+    "History",
+    "Horror",
+    "Music",
+    "Mystery",
+    "Romance",
+    "Science Fiction",
+    "Thriller",
+    "TV Movie",
+    "War",
+    "Western"
+]
+
+export default genres
