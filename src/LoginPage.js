f15af0a2bcb2d365ae930d5285a4bced3b4051ef
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-07-19 17:26:40 +0100

    Allowing user to login with saved data in user2

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,44 +1,67 @@
-import React from 'react'
-import { Button, Row, Col, Form, InputGroup } from 'react-bootstrap'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faKey } from "@fortawesome/free-solid-svg-icons"
+import React, { useState } from 'react';
+import { Button, Row, Col, Form, InputGroup } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faKey } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import data from './user2';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const user = data.users.find((user) => user.username === username && user.password === password);
+
+    if (user) {
+      user.loggedin = true;
+      localStorage.setItem('users', JSON.stringify(data.users));
+      window.location.href = '/ProfilePage';
+    } else {
+      setError('User not found or Incorrect Password');
+    }
+  };
+
   return (
     <Row className="login-page">
-      <h1 className='text-center'>Login!</h1>
-      <Col className='mx-5'>
-        <Form>
+      <h1 className="text-center">Login!</h1>
+      <Col className="mx-5">
+        <Form onSubmit={handleLogin}>
           <Form.Label>Username:</Form.Label>
-
           <InputGroup>
             <InputGroup.Text>
               <FontAwesomeIcon icon={faUser} />
             </InputGroup.Text>
-
-            <Form.Control type="text" placeholder="Enter username" />
+            <Form.Control type="text" placeholder="Enter username" value={username} onChange={(e) => setUsername(e.target.value)} />
           </InputGroup>
 
           <Form.Label>Password:</Form.Label>
           <InputGroup>
             <InputGroup.Text>
-                            <FontAwesomeIcon icon={faKey} />
+              <FontAwesomeIcon icon={faKey} />
             </InputGroup.Text>
-
-            <Form.Control type="password" placeholder="Enter password" />
+            <Form.Control type="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} />
           </InputGroup>
 
-          <Row className='justify-content-center'>
+          <Row className="justify-content-center">
             <Col lg="1">
-              <Button className='text-center mx-auto' variant="primary" type="submit">Login!</Button>
+              <Button className="text-center mx-auto" variant="primary" type="submit">
+                Login!
+              </Button>
             </Col>
           </Row>
-          
-        </Form>
-      </Col>
-      
-    </Row>
-  )
-}
 
-export default LoginPage
+          {error && <p className="text-danger mt-3">{error}</p>}
+        </Form>
+
+        <p className="mt-3">
+          Don't have an account? <Link to="/RegisterPage1">Register</Link>
+        </p>
+      </Col>
+    </Row>
+  );
+};
+
+export default LoginPage;
